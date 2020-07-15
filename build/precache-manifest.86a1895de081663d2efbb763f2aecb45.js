847a9aecd87fdd0afe59f2c1a695a4b45e9bd148
vasujain314 <jain.vasu509@gmail.com>
2020-07-15 16:34:04 +0530

    deploy

---
+++ build/precache-manifest.86a1895de081663d2efbb763f2aecb45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6447a9050cf208362acbb6cf056cfc7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24175b1f8e4fecd2f74e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "76784c9ca18b92cc9c16",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "24175b1f8e4fecd2f74e",
+    "url": "/static/js/2.a276cb48.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.a276cb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76784c9ca18b92cc9c16",
+    "url": "/static/js/main.9620be15.chunk.js"
+  },
+  {
+    "revision": "76c0879cc130edce90a2",
+    "url": "/static/js/runtime-main.2cea7679.js"
+  }
+]);
